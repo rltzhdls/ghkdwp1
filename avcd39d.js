e1f960d03b93b2f4bb9c7809b726faacbfebb977
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-30 13:11:11 +0900

    Add files via upload

---
+++ avcd39d.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/220525/couponbg.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
